7e93ec0c27a963b2c2311711111f70c4bce80f6a
Linus Blom <linus.blom@me.com>
2020-07-09 21:31:08 +0200

    add ping route

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -4,10 +4,14 @@ import player from './player';
 import user from './user';
 import game from './game';
 import jackpot from './jackpot';
+import ping from './ping';
+import { userId } from '../middlewares';
 
 export const router = new Router({ prefix: '/api/v1' });
 
 router
+  .use('/ping', ping.routes(), ping.allowedMethods())
+  .use(userId)
   .use('/player', player.routes(), player.allowedMethods())
   .use('/user', user.routes(), user.allowedMethods())
   .use('/game', game.routes(), game.allowedMethods())
